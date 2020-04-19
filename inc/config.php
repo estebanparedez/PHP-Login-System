2f4b55d5ee7624859f0716817754bef016d8bdfb
Esteban <estebanparedez@gmail.com>
2020-04-19 20:33:15 -0300

    Added confing: added login/reg pages; added footer

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+	// if there is no constante defined called __CONFIG__. do not load this file
+	if(!defined('__CONFIG__')) {
+		exit('You do not have a config file');
+	}
+
+
+	// our confi is below
+
+
+?>
